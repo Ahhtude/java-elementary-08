d2dd5a995261fcb6a97f9cfced51582c4b214edc
sergey <whteahhtude@gmail.com>
2019-04-29 12:26:59 +0300

    Екзаменационное задание

--- src/org/demo/birds/processor/UserCommandProcessor.java
+++ src/org/demo/birds/processor/UserCommandProcessor.java
@@ -10,44 +10,51 @@ import java.util.Scanner;
  * Реализовать интерфейсы IBirdCreator, IUserCommandProcessor, IInfiniteLoopProcessor
  */
 public class UserCommandProcessor  implements IBirdCreator,IInfiniteLoopProcessor,IUserCommandProcessor {
-    Scanner scanner = new Scanner(System.in);
 
     @Override
     public Bird createBird(Scanner userInputReader) {
         System.out.println("Please, enter bird name: ");
-        String name = scanner.nextLine();
+        String name = userInputReader.nextLine();
         System.out.println("Please, enter bird living area:");
-        String livingArea = scanner.nextLine();
+        String livingArea = userInputReader.nextLine();
         System.out.println("Please, enter bird size:");
-        double birdSize = scanner.nextDouble();
+        double birdSize = userInputReader.nextDouble();
         return new Bird(name,livingArea,birdSize);
+
     }
 
     @Override
     public void processInLoop() {
+        Scanner scanner = new Scanner(System.in);
         while (true){
             System.out.print("Please, enter command: \n" +
                     " a - add new Bird \n " +
                     "s - search bird by name \n" +
                     " l - search bird by living area \n" +
                     " exit - terminate application \n");
-            String commandInLoop = scanner.nextLine();
-            processUserCommand(commandInLoop,scanner);
+            processUserCommand(scanner.nextLine(),scanner);
         }
     }
 
     @Override
     public void processUserCommand(String command, Scanner userInputReader) {
-        if (command=="a"){
-        BirdStore.getSingtoneBirdStore().addBird(createBird(scanner));
+
+        if (command.equals("a")){
+        BirdStore.getSingltoneBirdStore().addBird(createBird(userInputReader));
         }
-        else if (command == "s"){
-            System.out.println("Please, enter bird living area to search: ");
-            BirdStore.getSingtoneBirdStore().searchByLivingArea(scanner.nextLine().toLowerCase());
+
+        else if (command.equals("s")){
+            System.out.println("Please, enter bird name to search: ");
+            System.out.println(
+            BirdStore.getSingltoneBirdStore().searchByName(userInputReader.nextLine()).getName() + " was find in BirdStore");
+
         }
-        else if (command == "l"){
+        else if (command.equals("l")){
             System.out.println("Please, enter bird living area to search: ");
-            BirdStore.getSingtoneBirdStore().searchByName(scanner.nextLine().toLowerCase());
+            BirdStore.getSingltoneBirdStore().searchByLivingArea(userInputReader.nextLine())
+                    .forEach(a->{
+                System.out.println(a.toString() + "Was find by living areas search");
+            });
         }
         else System.out.println("Unknown command.");
         }
--- src/org/demo/birds/store/BirdStore.java
+++ src/org/demo/birds/store/BirdStore.java
@@ -2,47 +2,59 @@ package org.demo.birds.store;
 
 import org.demo.birds.entities.Bird;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Отнаследоваться от AbstractBirdStore.
- *
+ * <p>
  * Реализовать паттерн Singleton.
  */
-public class BirdStore extends AbstractBirdStore {
-    Map birdStore = new HashMap();
 
-    private static BirdStore singtoneBirdStore = new BirdStore();
+public class BirdStore extends AbstractBirdStore {
+     private Map birdStore = new HashMap();
+
+    private static BirdStore singltoneBirdStore = new BirdStore();
 
     private BirdStore() {
     }
 
-    public static BirdStore getSingtoneBirdStore(){
-        return singtoneBirdStore;
+    public static BirdStore getSingltoneBirdStore() {
+        return singltoneBirdStore;
     }
 
     @Override
     public void addBird(Bird b) {
-        birdStore.put(b.getName().toLowerCase(), b.getLivingArea().toLowerCase());
+        birdStore.put(b, b.getLivingArea());
 
 
     }
 
     @Override
     public Bird searchByName(String nameToSearch) {
-        Bird searchObj;
-        searchObj = (Bird) birdStore.get(nameToSearch);
-        return searchObj;
+        Bird searchBird = null;
+        for (Object b : birdStore.keySet()) {
+            if (
+                    ((Bird) b).getName().equals(nameToSearch)) {
+                searchBird = (Bird) b;
+            }
+        }
+        return  searchBird;
+
+
     }
 
     @Override
     public List searchByLivingArea(String livingAreaToFind) {
         List listOfBirdsByArea = new ArrayList();
-        for (Object names : birdStore.keySet()) {
-            if (birdStore.get(((String) names).toLowerCase()) == listOfBirdsByArea) {
-                listOfBirdsByArea.add(names);
+        for (Object obj : birdStore.keySet()) {
+            if (((Bird) obj).getLivingArea().equals(livingAreaToFind)) {
+                listOfBirdsByArea.add(obj);
             }
         }
         return listOfBirdsByArea;
+
     }
 }
